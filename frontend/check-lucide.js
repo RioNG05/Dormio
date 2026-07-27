96e097e12212abef4c60888c721782ddbb490070
RioNguyen <rionguyencontactwork@gmail.com>
2026-07-27 21:29:41 +0700

    Delete frontend directory

--- frontend/check-lucide.js
+++
@@ -1,5 +0,0 @@
-const lucide = require('lucide-react');
-console.log(Object.keys(lucide).includes('Camera'));
-console.log(Object.keys(lucide).includes('ImageIcon'));
-console.log(Object.keys(lucide).includes('Image'));
-console.log(Object.keys(lucide).includes('MoreVertical'));
